4239cf424f8e724a3b08ef0681fd035ea5a1ef3e
alch29 <christensen.alek@gmail.com>
2025-08-17 22:57:39 +0200

    .

--- portfolio.js
+++ portfolio.js
@@ -5,3 +5,34 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 })
+
+fetch('logo-og-visitkort-jyderup-haveservice.html')
+  .then(response => response.text())
+  .then(data => {
+    // Opret et midlertidigt DOM-objekt for at kunne søge i den hentede HTML
+    let tempDiv = document.createElement('div');
+    tempDiv.innerHTML = data;
+
+    // Find elementet med den ønskede class
+    let textElement = tempDiv.querySelector('.portfolio-item-text-full');
+
+    const createExcerpt = (content, maxNumberOfWords, trailingIndicator = '...') => {
+      const listOfWords = content.trim().split(' ');
+      const truncatedContent = listOfWords.slice(0, maxNumberOfWords).join(' ');
+      const excerpt = truncatedContent + trailingIndicator;
+      const output = listOfWords.length > maxNumberOfWords ? excerpt : content;
+      
+      return output;
+    };
+
+    let excerpt = createExcerpt(textElement.innerText, 15);
+
+    // Sæt indholdet ind i din container
+    document.querySelector('.text-container').innerHTML =
+      `<p class="portfolio-item-text">${excerpt}</p>`;
+
+  })
+  .catch(error => console.error('Fejl ved indlæsning:', error));
+
+
+//https://medium.com/@paulohfev/problem-solving-how-to-create-an-excerpt-fdb048687928
